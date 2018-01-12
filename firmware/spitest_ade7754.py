4613d2e67e179f682984766ecd29f2979442c1f2
Stefan Jost <stjost@edu.aau.at>
2018-01-12 03:36:46 +0100

    working spitest_ade7753.py

--- firmware/spitest_ade7754.py
+++ firmware/spitest_ade7754.py
@@ -1,13 +1,13 @@
 import spidev
-import time
 
 print "Try to setup SPI..."
 spi = spidev.SpiDev()
 spi.open(0,0)
-spi.max_speed_hz = 10000000
+spi.max_speed_hz = 62500
+spi.mode = 0b01
 
 print "Try to send via SPI..."
-while True:
-    spi.xfer([0x3F])
-    spi.readbytes(1)
+to_send = [0x1C, 0x00, 0x00]
+print to_send
+print spi.xfer2(to_send)
 print "Programm successfuly ended!"
